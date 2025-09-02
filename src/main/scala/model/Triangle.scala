b0e6a4eb882e039dec2e86bbed7eede13991a0bf
xDavidRTx <dpal.pereira@gmail.com>
2025-09-02 18:27:52 +0100

    Join models
    Path was too small to be left alone

--- src/main/scala/model/Triangle.scala
+++ src/main/scala/model/Triangle.scala
@@ -1,5 +1,16 @@
 package model
 
+case class Node(value: Int) extends AnyVal
+
+case class NodePath(sum: Int, path: Vector[Node]) {
+  def addNode(node: Node): NodePath = NodePath(sum + node.value, node +: path)
+
+  def encodePretty: String = {
+    val pathStr = path.map(_.value).mkString(" + ")
+    s"Minimal path is: $pathStr = $sum"
+  }
+}
+
 case class TriangleRow(nodes: Vector[Node])
 
 case class Triangle(rows: Vector[TriangleRow]) {
